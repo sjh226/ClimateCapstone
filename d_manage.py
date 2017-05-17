9370e13c90bbb1220a013ccdd802bc9e8da10fac
Steve Harshman <sharshman193@gmail.com>
2017-05-16 20:00:37 -0600

    starting to fill NaNs

--- d_manage.py
+++ d_manage.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import re
+pd.options.mode.chained_assignment = None
 
 
 def camel_to_snake(name):
@@ -39,7 +40,9 @@ def clean_data(dataframe):
                                 'MonthlyGreatestSnowDepthDate',\
                                 'REPORTTPYE', 'DAILYWeather',\
                                 'HOURLYPRSENTWEATHERTYPE', 'DAILYSunrise',\
-                                'DAILYSunset', 'HOURLYSKYCONDITIONS',],\
+                                'DAILYSunset', 'HOURLYSKYCONDITIONS',\
+                                'HOURLYDRYBULBTEMPC', 'HOURLYWETBULBTEMPC',\
+                                'HOURLYDewPointTempC', 'HOURLYWindDirection'],\
                                 axis=1)
     # possible climate change indicators
     climate_df = dataframe[['MonthlyDeptFromNormalMaximumTemp',\
@@ -55,8 +58,30 @@ def clean_data(dataframe):
     date_df = dataframe.pop('DATE')
     # convert camelcase to snakecase for columns
     dataframe.columns = [camel_to_snake(name) for name in dataframe.columns]
+    dataframe.columns = ['hourly_visibility', 'hourly_dry_bulb_temp_f',\
+                         'hourly_wet_bulb_temp_f'] + list(dataframe.columns[3:])
     return dataframe, climate_df, date_df
+
+def fill_nans(df):
+    # drop rows with fewer than 10 non-na values (78% of data)
+    df = df.dropna(thresh=10)
+    # drop rows with NaN in largest column
+    # df = df.dropna(subset=['hourly_dry_bulb_temp_f'])
+    # fill hourly visibility NaNs with mean
+    df['hourly_visibility'].loc[:] = df['hourly_visibility'].astype(str)\
+                                     .str.rstrip('sV').replace('*', 'nan')
+    mean_vis = np.mean(df[df['hourly_visibility'] != 'nan']['hourly_visibility']\
+               .astype(float))
+    df['hourly_visibility'] = df['hourly_visibility'].str.replace('nan',str(mean_vis))\
+                              .astype(float)
+
+    df['hourly_precip'].fillna('0.00', inplace=True)
+    df['hourly_precip'] = df['hourly_precip'].str.rstrip('s').replace('T', '0.00')
+    df['hourly_precip'] = df['hourly_precip'].astype(float)
+    
+    return df
 
 if __name__ == '__main__':
     df = pd.read_csv('965113.csv')
     df, climate_df, date_df = clean_data(df)
+    df = fill_nans(df)
