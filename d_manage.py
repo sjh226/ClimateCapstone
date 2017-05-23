034f2ce1ca1d22678b84145ecbebb87045c871e5
Steve Harshman <sharshman193@gmail.com>
2017-05-23 10:02:47 -0600

    made the first KNN imputation workgit add d_manage.py

--- d_manage.py
+++ d_manage.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import math
 import re
+from sklearn.neighbors import KNeighborsClassifier
+
 pd.options.mode.chained_assignment = None
 
 
@@ -65,7 +67,7 @@ def clean_data(dataframe):
     return dataframe, climate_df
 
 def clean_type(df):
-    # how do I keep values as NaN when converting?
+    # convert columns to consistent types (float or datetime)
     columns = ['hourly_visibility', 'hourly_dry_bulb_temp_f', 'hourly_wet_bulb_temp_f',\
                'hourly_wet_bulb_temp_f', 'hourly_dew_point_temp_f',\
                'hourly_relative_humidity', 'hourly_wind_speed',\
@@ -75,20 +77,17 @@ def clean_type(df):
                'daily_dept_from_normal_average_temp', 'daily_precip',\
                'daily_snowfall', 'daily_snow_depth',\
                'daily_peak_wind_speed', 'daily_heating_degree_days',\
-               'daily_cooling_degree_days']
+               'daily_cooling_degree_days', 'date']
     for col in columns:
         df[col] = pd.to_numeric(df[col], errors='coerce')
-    # strings = df['hourly_dry_bulb_temp_f'].astype(str)
-    # for string in strings:
-    #     new.append(re.sub('\D\W', '', s))
     return df
 
 
 def fill_nans(df):
+    # drop rows with NaN in largest column aside from 'date'
+    df = df.dropna(subset=['hourly_relative_humidity'])
     # drop rows with fewer than 10 non-na values (78% of data)
-    df = df.dropna(thresh=10)
-    # drop rows with NaN in largest column
-    # df = df.dropna(subset=['hourly_dry_bulb_temp_f'])
+    # df = df.dropna(thresh=10)
     ## fill hourly visibility NaNs with mean
     # df['hourly_visibility'].loc[:] = df['hourly_visibility'].astype(str)\
     #                                  .str.rstrip('sV').replace('*', 'nan')
@@ -97,30 +96,48 @@ def fill_nans(df):
     # df['hourly_visibility'] = df['hourly_visibility'].str.replace('nan',str(mean_vis))\
     #                           .astype(float)
 
-    df['hourly_precip'].fillna('0.00', inplace=True)
-    df['hourly_precip'] = df['hourly_precip'].str.rstrip('s').replace('T', '0.00')
-    df['hourly_precip'] = df['hourly_precip'].astype(float)
-
-    # use monthly averages to fill nans
-    mean_temps = np.array([])
-    for idx, temp in enumerate(df['monthly_mean_temp']):
-        if not np.isnan(temp):
-            if len(mean_temps) > 1:
-                mean_temps = np.vstack((mean_temps, np.array([idx, temp])))
-            else:
-                mean_temps = np.hstack((mean_temps, np.array([idx, temp])))
-    for row in df.head().iterrows():
-        idx = row[0]
-        row[1]['hourly_dry_bulb_temp_f']
-
     # try KNN to impute values
     # https://github.com/chrisalbon/notes_on_data_science_machine_learning_and_artificial_intelligence
+    columns = ['hourly_dry_bulb_temp_f']
+    complete_columns = ['hourly_dew_point_temp_f', 'hourly_relative_humidity', \
+                        'date']
+    for col in columns:
+        KNN = KNeighborsClassifier(weights='distance')
+        X = df[df[col].notnull()][complete_columns]
+        print(X.info())
+        X_pred = df[df[col].isnull()][complete_columns].values
+        y = df[df[col].notnull()].pop(col).values
+        X = X.values
+        KNN.fit(X, y)
+        print(KNN.predict(X_pred))
 
+
+
+    # fill precip nans to 0.00
+    # df['hourly_precip'].fillna('0.00', inplace=True)
+
+    # # use monthly averages to fill nans
+    # mean_temps = np.array([])
+    # for idx, temp in enumerate(df['monthly_mean_temp']):
+    #     if not np.isnan(temp):
+    #         if len(mean_temps) > 1:
+    #             mean_temps = np.vstack((mean_temps, np.array([idx, temp])))
+    #         else:
+    #             mean_temps = np.hstack((mean_temps, np.array([idx, temp])))
+    # for row in df.head().iterrows():
+    #     idx = row[0]
+    #     row[1]['hourly_dry_bulb_temp_f']
 
     return df
 
 if __name__ == '__main__':
     df = pd.read_csv('965113.csv')
     df, climate_df = clean_data(df)
-    # df = fill_nans(df)
     df = clean_type(df)
+
+    df1 = df[['date','hourly_visibility','hourly_dry_bulb_temp_f',\
+              'hourly_wet_bulb_temp_f','hourly_dew_point_temp_f',\
+              'hourly_relative_humidity','hourly_wind_speed',\
+              'hourly_wind_gust_speed','hourly_station_pressure']].head(1000)
+
+    df1 = fill_nans(df1)
