0df8e0ce4729e2d5a4286bb060831b70f6e715e8
Steve Harshman <sharshman193@gmail.com>
2017-05-24 16:56:56 -0600

    graph updates

--- eda.py
+++ eda.py
@@ -11,13 +11,15 @@ def plot_departure(df, x_col, y_col, title, file_name):
     plt.close()
     y = df[y_col].values
     x = df[x_col].values
+    x = x.reshape(-1, 1)
+    y = y.reshape(-1, 1)
     min_max_scaler = preprocessing.MinMaxScaler()
     x = min_max_scaler.fit_transform(x)
     plt.scatter(x, y, color='black', s=.3, marker='o')
     X_test, y_test, lr = lin_reg(x, y)
     plt.plot(X_test, lr.predict(X_test), color='red', linewidth=1)
     plt.title(title)
-    plt.xlabel('Dates from Jan 1, 1997 to Today')
+    plt.xlabel('Time since Jan 1, 1997')
     plt.ylabel('Temperature (F)')
     plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
     plt.savefig('{}.png'.format(file_name))
@@ -26,10 +28,6 @@ def lin_reg(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, \
                                                         random_state=13)
     lr = LinearRegression()
-    X_train = X_train.reshape(-1, 1)
-    X_test = X_test.reshape(-1, 1)
-    y_train = y_train.reshape(-1, 1)
-    y_test = y_test.reshape(-1, 1)
     lr.fit(X_train, y_train)
     print('Score: {}'.format(lr.score(X_test, y_test)))
     return X_test, y_test, lr
