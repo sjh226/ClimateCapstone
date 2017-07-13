3bfe1c2aa8d52a2acdf3287caa8d35d69d5a31ea
Steve Harshman <sharshman193@gmail.com>
2017-07-13 15:57:29 -0600

    fixing prediction graphs

--- web_app/app.py
+++ web_app/app.py
@@ -19,21 +19,25 @@ def predict():
         x = str(request.form['Date'])
         y, std = pred_one(model, x)
         if len(x) == 10:
-            years = list(range(2010, (int(x[:4]) + 1)))
+            years = list(range(2000, (int(x[:4]) + 1)))
+            std_years = list(range(2017, (int(x[:4]) + 1)))
             dates = ['{}{}'.format(year, x[4:]) for year in years]
+            std_dates = ['{}{}'.format(year, x[4:]) for year in std_years]
             y_preds = np.array([pred_one(model, str(date))[0] for date in dates])
-            stds = np.array([pred_one(model, str(date))[1][0] for date in dates])
-            print(y_preds, stds)
+            std_preds = np.array([pred_one(model, str(date))[0] for date in std_dates])
+            stds = np.array([pred_one(model, str(date))[1][0] for date in std_dates])
+            print(std_preds, stds)
             plt.close()
-            plt.plot(years, y_preds)
-            plt.fill(np.concatenate([years, years[::-1]]),\
-                     np.concatenate([y_preds - 1.96 * stds, (y_preds + 1.96 * stds)[::-1]]),\
+            plt.plot(years, y_preds, c='r')
+            plt.fill(np.concatenate([std_years, std_years[::-1]]),\
+                     np.concatenate([std_preds - 1.96 * stds, (std_preds + 1.96 * stds)[::-1]]),\
                      alpha=.5, fc='gray', ec='None', label='95% confidence interval')
-            plt.title('Prediction for {} Since 2010'.format(x[5:]))
+            plt.title('Prediction for {} Since 2000'.format(x[5:]))
             plt.xlabel('Date')
             plt.ticklabel_format(style='plain', axis='x')
             plt.ylabel('Temperature (C)')
-            plt.legend()
+            if int(x[:4]) > 2017:
+                plt.legend()
             plt.tight_layout()
             plt.savefig('static/img/pred_{}.png'.format(x))
             return render_template('predict.html', y_pred=y, Date=x)
